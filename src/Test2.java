b7a67c65859b55e7ed60f8a5e54c26fa9df90a95
lnphuongmai <lnphuongmai07@gmail.com>
2026-03-25 22:10:42 +0700

    try on my own branch

--- src/Test2.java
+++ src/Test2.java
@@ -1,4 +1,8 @@
 package src;
 
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test2");
+        System.out.println("Test2" + " lnpmai");
+    }
 }
--- test/src/BankAccount.java
+++ test/src/BankAccount.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+    public static void main(String[] args) {
+        System.out.println("Give me your Account");
+    }
 }
